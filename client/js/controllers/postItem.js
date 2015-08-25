c66eedb1fa3cfb32e37559b4e21904b60ca44643
unknown <adityasanghi1992@gmail.com>
2015-08-24 22:20:43 -0400

    Post mock

--- client/js/controllers/postItem.js
+++ client/js/controllers/postItem.js
@@ -10,6 +10,24 @@ angular.module('app.ctrl.postItem', ['ui.bootstrap'])
                                     ){
 
     //Initialize 
+    $scope.sizes = [
+          "small (12-inch)",
+          "medium (14-inch)",
+          "large (16-inch)",
+          "insane (42-inch)"
+      ];
+      
+      $scope.toppings = [
+        { category: 'meat', name: 'Pepperoni' },
+        { category: 'meat', name: 'Sausage' },
+        { category: 'meat', name: 'Ground Beef' },
+        { category: 'meat', name: 'Bacon' },
+        { category: 'veg', name: 'Mushrooms' },
+        { category: 'veg', name: 'Onion' },
+        { category: 'veg', name: 'Green Pepper' },
+        { category: 'veg', name: 'Green Olives' }
+      ];
+
     if(!$scope.item){
         $scope.item = {category: null};
     }
